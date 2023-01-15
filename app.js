219678b90c4d61012168fd50ef6c051ad39af4dd
MicaelaDuque <micaduque@gmail.com>
2023-01-15 16:29:32 -0300

    agregando-mercado-liebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 2000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get('/register', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/login', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`server corriendo en ${PORT}`);
+});
